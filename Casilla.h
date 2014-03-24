fa91d383d9d0c2d8f5080ec261fb892e3ba42527
jricardobrenes <jricardobrenes@gmail.com>
2014-03-23 20:10:37 -0600

    Create Casilla.h

---
+++ Casilla.h
@@ -0,0 +1,13 @@
+#ifndef CASILLA_H  
+#define CASILLA_H
+
+typedef struct Casilla{
+  char paredes;
+  int valor;
+  /*Casilla(char _paredes, int _valor){
+     paredes = _paredes;
+     valor = _valor;
+  }*/
+}Casilla;
+
+#endif
